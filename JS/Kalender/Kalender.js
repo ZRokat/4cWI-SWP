870cca9f6c04a9e7574dfb5d3a17e55e6776f215
ErixXxYe <derstylervonmorgen@gmail.com>
2021-11-24 10:04:30 +0100

    Next month funktioniert

--- JS/Kalender/Kalender.js
+++ JS/Kalender/Kalender.js
@@ -1,13 +1,25 @@
 var id = 1;
 var idString = id.toString();
+var monthId = 0;
 const months = [
-    {"name": "Januar", "days": 30},
-    "Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Detember"]
+    {"name": "Januar", "days": 31},
+    {"name": "Februar", "days": 28},
+    {"name": "März", "days": 31},
+    {"name": "April", "days": 30},
+    {"name": "Mai", "days": 31},
+    {"name": "Juni", "days": 30},
+    {"name": "Juli", "days": 31},
+    {"name": "August", "days": 31},
+    {"name": "September", "days": 30},
+    {"name": "Oktober", "days": 31},
+    {"name": "November", "days": 30},
+    {"name": "Dezember", "days": 31},
+    ]
 var offset = 4;
 var nextOffset = 0;
 
 function startCalendar() {
-    document.getElementById("month").innerHTML = months[0].name;
+    document.getElementById("month").innerHTML = months[monthId].name;
     while (offset > 0){
         document.getElementById(idString).innerHTML = "-";
         offset--;
@@ -62,6 +74,7 @@ function previousMonth(){
 
 
 function nextMonth(){
+    monthId++;
     let offsetAlreadySet = false;
     offset = nextOffset;
     nextOffset = 0;
@@ -70,14 +83,14 @@ function nextMonth(){
     if (month != 11){
         month ++;   
     }
-    document.getElementById("month").innerHTML = months[month];
+    document.getElementById("month").innerHTML = months[monthId].name;
     while (offset > 0){
         document.getElementById(idString).innerHTML = "-";
         offset--;
         id++;
         idString = id.toString();
     }
-    for (let trueDays = 1; trueDays <= days; trueDays++){
+    for (let trueDays = 1; trueDays <= months[monthId].days; trueDays++){
     document.getElementById(idString).innerHTML = trueDays;
     id++;
     idString = id.toString();
