1f6876a8da9be3c8b579f8c70c60e81cdf7b9a03
ErixXxYe <derstylervonmorgen@gmail.com>
2021-11-24 10:45:41 +0100

    Kleiner refaktor

--- JS/Kalender/Kalender.js
+++ JS/Kalender/Kalender.js
@@ -17,19 +17,24 @@ const months = [
     ]
 var offset = 4;
 var nextOffset = 0;
+var lastOffset = 0;
+
+function nextDay() {
+    id++;
+    idString = id.toString();
+}
 
 function startCalendar() {
     document.getElementById("month").innerHTML = months[monthId].name;
     while (offset > 0){
         document.getElementById(idString).innerHTML = "-";
         offset--;
-        id++;
-        idString = id.toString();
+        lastOffset++;
+        nextDay();
     }
     for (let trueDays = 1; trueDays <= months[0].days; trueDays++){
     document.getElementById(idString).innerHTML = trueDays;
-    id++;
-    idString = id.toString();
+    nextDay();
     }
     while (id <= 42){
         if (id <= 35){ 
@@ -37,63 +42,65 @@ function startCalendar() {
             console.log(nextOffset);  
         }
         document.getElementById(idString).innerHTML = "-";
-        id++;
-        idString = id.toString(); 
+        nextDay();
     }
 }
 
 function previousMonth(){
-        if (month != 0){
-            month --;   
-        }
-        document.getElementById("month").innerHTML = months[month];
-        while (offset > 0){
-            document.getElementById(idString).innerHTML = "-";
-            offset--;
-            id++;
-            idString = id.toString();
-        }
-        for (let trueDays = 1; trueDays <= days; trueDays++){
-        document.getElementById(idString).innerHTML = trueDays;
-        id++;
-        idString = id.toString();
-        }
-        while (id <= 42){
-            if (id <= 35){
-                offset++;
-            }
-            if (id > 35 && id < 42){
-                offset++;
-            }
-            document.getElementById(idString).innerHTML = "-";
-            id++;
-            idString = id.toString();
-            
-        }
+    if (monthId != 0){
+        monthId--;   
+    let offsetAlreadySet = false;
+    offset = lastOffset;
+    lastOffset = 0;
+    nextOffset = 0;
+    id = 1;
+    idString = id.toString();
+    document.getElementById("month").innerHTML = months[monthId].name;
+    while (offset > 0){
+        document.getElementById(idString).innerHTML = "-";
+        offset--;
+        lastOffset++;
+        nextDay();
     }
+    for (let trueDays = 1; trueDays <= months[monthId].days; trueDays++){
+    document.getElementById(idString).innerHTML = trueDays;
+    nextDay();
+    }
+    while (id <= 42){
+        if (id <= 35){
+            
+            console.log(nextOffset);
+            offsetAlreadySet = true;
+        }
+        if (offsetAlreadySet == false){
+            console.log(nextOffset);
+        }
+        document.getElementById(idString).innerHTML = "-";
+        nextDay();        
+    }
+    }
+}
 
 
 function nextMonth(){
-    monthId++;
+    if (monthId != 11){
+        monthId++;  
     let offsetAlreadySet = false;
     offset = nextOffset;
     nextOffset = 0;
     id = 1;
     idString = id.toString();
-    if (month != 11){
-        month ++;   
-    }
+    
     document.getElementById("month").innerHTML = months[monthId].name;
     while (offset > 0){
         document.getElementById(idString).innerHTML = "-";
         offset--;
-        id++;
-        idString = id.toString();
+        lastOffset++;
+        nextDay();
     }
     for (let trueDays = 1; trueDays <= months[monthId].days; trueDays++){
     document.getElementById(idString).innerHTML = trueDays;
-    id++;
-    idString = id.toString();
+    nextDay();
     }
     while (id <= 42){
         if (id <= 35){
@@ -106,10 +113,10 @@ function nextMonth(){
             console.log(nextOffset);
         }
         document.getElementById(idString).innerHTML = "-";
-        id++;
-        idString = id.toString();
+        nextDay();
         
     }
+}
 }
 
 startCalendar();
