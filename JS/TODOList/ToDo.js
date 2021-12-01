45e22f377c6c4c5bf2f2a6067cb4658781d2ba56
ErixXxYe <derstylervonmorgen@gmail.com>
2021-12-01 11:07:13 +0100

    ToDO List bsp 1 fertig

---
+++ JS/TODOList/ToDo.js
@@ -0,0 +1,22 @@
+const tasks = ["putzen","tanzen","kochen"];
+
+printTaskList();
+
+document.getElementById("Addtask").addEventListener("click",function(){
+    let newTask = document.getElementById("newTask").value;
+    console.log(newTask)
+    tasks.push(newTask);
+    printTaskList();
+});
+
+function printTaskList(){
+    document.getElementById("TaskList").innerHTML = getHTMLTasks();
+}
+
+function getHTMLTasks(){
+    let html = "";
+    tasks.forEach(element => {
+        html += "<li>" + element + "</li>"   
+    });
+    return html;  
+}
