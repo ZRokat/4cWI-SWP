53d4877da43ac48d40ea12f246ccb5710cc4211d
ErixXxYe <derstylervonmorgen@gmail.com>
2022-03-02 11:04:42 +0100

    First project erstellt und Buttons gemacht

---
+++ REACT/firstreactproject/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SuperButton from './Components/superbutton';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Huen</h1>
+        <SuperButton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/firstreactproject/src/Components/superbutton.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+export default class SuperButton extends Component{
+    render(){
+        return (<div>
+            <button>Button</button>
+        </div>)
+    }
+}
